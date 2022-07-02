ba05e6964bd70d7e261a72a79dd4e4a0b5a2e5b1
istywork <myungs8562@gmail.com>
2022-07-02 14:04:56 +0900

    update

---
+++ baekjoon/6549.py
@@ -0,0 +1,24 @@
+import sys
+import collections
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+h = list(map(int, input().rstrip().split()))
+n = h.pop(0)
+
+if n == 0:
+    exit(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    uhg
