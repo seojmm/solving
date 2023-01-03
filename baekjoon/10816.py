c681bd7e2f2ebb26915148dab96f425703a5271b
seojm <myungs8562@gmail.com>
2023-01-03 14:13:12 +0900

    problem set

--- baekjoon/10816.py
+++ baekjoon/10816.py
@@ -6,24 +6,5 @@ N = int(input())
 card = list(map(int, input().split()))
 M = int(input())
 required = list(map(int, input().split()))
-ans = [0]*M
+ans = {}
 
-card.sort()
-
-
-for i in range(M):
-    l, r = 0, N
-    while l<r:
-        mid = (r-l)//2 + l
-        
-        if card[mid] < required[i]:
-            l = mid + 1
-        else:
-            r = mid
-            
-    j = r
-    while j < N and card[j] == required[i]:
-        ans[i] += 1
-        j += 1
-
-print(*ans)
